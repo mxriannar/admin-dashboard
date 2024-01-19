74ecb64401b3b30c88f6d27295b036e643ee54ad
Marianna Rodrigues <mxriannar@gmail.com>
2024-01-19 00:02:57 -0300

    Criando a página de gerenciamento do time.

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -50,7 +50,7 @@ const Sidebar = () => {
           backgroundColor: "transparent !important",
         },
         "& .pro-inner-item": {
-          padding: "5px 35px 5px 20px !important",
+          padding: "5px 20px 5px 20px !important",
         },
         "& .pro-inner-item:hover": {
           color: "#868dfb !important",
