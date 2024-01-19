b3e9c02d56c327a3ab706a77c13f68505a35d8c5
Marianna Rodrigues <mxriannar@gmail.com>
2024-01-18 21:11:05 -0300

    Criando e estilizando o topbar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard"
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -8,7 +11,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
